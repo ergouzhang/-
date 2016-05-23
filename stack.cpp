1ff7b7f6d9e51b022e64b5cbf100f6f77410090b
ergouzhang <zwjioro@163.com>
2016-05-23 21:00:36 +0800

    栈

--- stack.cpp
+++ stack.cpp
@@ -5,6 +5,7 @@
 
 #define MAX_SIZE_STACK 100;
 
+//线性表栈结构
 typedef struct sqStack
 {
     char *top,*base;
@@ -85,124 +86,252 @@ int StackLen(sqStack s)
 }
 
 
+/*
+//链栈
+typedef struct stackNode
+{
+    int data;
+    struct stackNode *next;
+
+}stackNode,*LinkStackPtr;
+
+typedef struct linkStack
+{
+    stackNode *top;
+    int count=0;
+}linkStack;
+
+ 
+ //init
+ 
+ void initlinkStack(linkStack *s)
+ {
+ s->top=NULL;
+ }
+//入栈
+void pushlinkStack(linkStack *s,int e)
+{
+    stackNode *p=(stackNode*)malloc(sizeof(stackNode));
+    p->data=e;
+    p->next=s->top;
+    s->top=p;
+    s->count++;
+
+}
+
+//出栈
+void poplinkStack(linkStack *s, int *e)
+{
+    if (s->count<=0) {
+        return;
+    }
+    
+    stackNode *p;
+    *e=s->top->data;
+    p=s->top;
+    s->top=s->top->next;
+
+    free(p);
+    s->count--;
+}
+*/
+//————————————————————————————————————————————————————
 //2 转10
-int binarytoTen()
-{
-    sqStack s;
-    char c;
-    int len,i,sum=0;
-    initStack(&s);
-    printf("输入二进制，输入#结束\n");
-    scanf("%c",&c);
-    while (c != '#') {
-        push(&s, c);
-        scanf("%c",&c);
-    }
-    
-    getchar();
-    
-    len=StackLen(s);
-    printf("stack length is : %d\n",len);
-    
-    for (i=0; i<len; i++) {
-        pop(&s, &c);
-        sum=sum+(c-48)*pow(2, i);
-    }
-    printf("10 is :  %d",sum);
-    return  sum;
-}
+//int binarytoTen()
+//{
+//    sqStack s;
+//    char c;
+//    int len,i,sum=0;
+//    initStack(&s);
+//    printf("输入二进制，输入#结束\n");
+//    scanf("%c",&c);
+//    while (c != '#') {
+//        push(&s, c);
+//        scanf("%c",&c);
+//    }
+//
+//    getchar();
+//
+//    len=StackLen(s);
+//    printf("stack length is : %d\n",len);
+//
+//    for (i=0; i<len; i++) {
+//        pop(&s, &c);
+//        sum=sum+(c-48)*pow(2, i);
+//    }
+//    printf("10 is :  %d",sum);
+//    return  sum;
+//}
+//
+//
+//————————————————————————————————————————————————————
+////2转8
+//int  main00()
+//{
+//    sqStack *s;
+//    char c;
+//    int len,sum=0;
+//    initStack(s);
+//    printf("输入二进制：\n");
+//    scanf("%c",&c);
+//    while (c!='#') {
+//
+//        push(s, c);
+//        scanf("%c",&c);
+//    }
+//    getchar();
+//
+//    len=StackLen(*s);
+//
+//    sqStack *l;
+//    initStack(l);
+//
+//    for (int i=0; i<len/3; i++) {
+//
+//        for (int j=0; j<3; j++) {
+//            pop(s, &c);
+//            sum=sum+(c-'0')*pow(2,j);
+//
+//        }
+//          push(l, sum+'0');  //数字转换成char
+//        sum=0;
+//    }
+//
+//    int lens=StackLen(*l);
+//      char a;
+//    for (int i=0; i<lens; i++) {
+//
+//        pop(l, &a);
+//        printf("===%c \n",a);
+//    }
+//
+//
+//    return 0;
+//}
+//
+//————————————————————————————————————————————————————
+////2-16
+//int nomain()
+//{
+//
+//    sqStack *s;
+//    char c;
+//    int len,sum=0;
+//    initStack(s);
+//    printf("输入二进制：\n");
+//    scanf("%c",&c);
+//    while (c!='#') {
+//
+//        push(s, c);
+//        scanf("%c",&c);
+//    }
+//    getchar();
+//
+//    len=StackLen(*s);
+//
+//    sqStack *l;
+//    initStack(l);
+//    char e;
+//    for (int i=0; i<len/3; i++) {
+//
+//        for (int j=0; j<4; j++) {
+//            pop(s, &c);
+//            sum=sum+(c-'0')*pow(2,j);
+//            if (sum>=10) {
+//                e=(sum-10)+'A';
+//            }
+//            else{  e=sum+48;}
+//
+//        }
+//        push(l, e);
+//        sum=0;
+//    }
+//
+//    int lens=StackLen(*l);
+//    char a;
+//    for (int i=0; i<lens; i++) {
+//
+//        pop(l, &a);
+//        printf("= %c \n",a);
+//    }
+//
+//
+//    return 0;
+//}
+//————————————————————————————————————————————————————
+//逆波兰表达式
 
+//int main()
+//{
+//    sqStack *s;
+//    initStack(s);
+//
+//    char str[100];
+//    char c;
+//    double d,e;
+//    int i=0;
+//    printf("input:");
+//    scanf("%c",&c);
+//
+//
+//    while (c!='#')  //输入结束判断
+//    {
+//        //输入是否为数字或者为'.'时，利用atof()转换为double型。
+//        while (isdigit(c) || c=='.')
+//        {
+//            str[i++]=c;
+//            str[i]=0;
+//            scanf("%c",&c);
+//            //c为空格，进行转换
+//            if (c==' ')
+//            { d=atof(str);//char[] to double;
+//                push(s, d);
+//                i=0;
+//                break;
+//            }
+//
+//        }
+//
+//        //c为运算符号时
+//        switch (c) {
+//            case '+':
+//                pop(s, &e);
+//                pop(s, &d);
+//                push(s, d+e);
+//                break;
+//            case '-':
+//                pop(s, &e);
+//                pop(s, &d);
+//                push(s, d-e);
+//                break;
+//            case '*':
+//                pop(s, &e);
+//                pop(s, &d);
+//                push(s, d*e);
+//                break;
+//            case '/':
+//                pop(s, &e);
+//                pop(s, &d);
+//                if (e==0) {
+//                    printf("error! 0\n");
+//                }
+//                push(s, d/e);
+//                break;
+//            default:
+//                break;
+//        }
+//
+//       scanf("%c",&c);
+//
+//    }
+//
+//
+//    pop(s, &d);
+//    printf("reuslt is %lf",d);
+//
+//    return 0;
+//}
 
-//2转8
-int  main()
-{
-    sqStack *s;
-    char c;
-    int len,sum=0;
-    initStack(s);
-    printf("输入二进制：\n");
-    scanf("%c",&c);
-    while (c!='#') {
+//----------------------------------
 
-        push(s, c);
-        scanf("%c",&c);
-    }
-    getchar();
-    
-    len=StackLen(*s);
-    
-    sqStack *l;
-    initStack(l);
-    
-    for (int i=0; i<len/3; i++) {
-        
-        for (int j=0; j<3; j++) {
-            pop(s, &c);
-            sum=sum+(c-'0')*pow(2,j);
-          
-        }
-          push(l, sum+'0');  //数字转换成char
-        sum=0;
-    }
-
-    int lens=StackLen(*l);
-      char a;
-    for (int i=0; i<lens; i++) {
-      
-        pop(l, &a);
-        printf("===%c \n",a);
-    }
-
-    
-    return 0;
-}
-
-//2-16
-int nomain()
-{
-    
-    sqStack *s;
-    char c;
-    int len,sum=0;
-    initStack(s);
-    printf("输入二进制：\n");
-    scanf("%c",&c);
-    while (c!='#') {
-        
-        push(s, c);
-        scanf("%c",&c);
-    }
-    getchar();
-    
-    len=StackLen(*s);
-    
-    sqStack *l;
-    initStack(l);
-    char e;
-    for (int i=0; i<len/3; i++) {
-        
-        for (int j=0; j<4; j++) {
-            pop(s, &c);
-            sum=sum+(c-'0')*pow(2,j);
-            if (sum>=10) {
-                e=(sum-10)+'A';
-            }
-            else{  e=sum+48;}
             
-        }
-        push(l, e);
-        sum=0;
-    }
-    
-    int lens=StackLen(*l);
-    char a;
-    for (int i=0; i<lens; i++) {
-        
-        pop(l, &a);
-        printf("= %c \n",a);
-    }
-    
-
-    return 0;
-}
-
---
+++ 栈的3种类型.cpp
@@ -0,0 +1,138 @@
+//
+//  c++.cpp
+//  c++
+//
+//  Created by zwj on 16/5/20.
+//  Copyright © 2016年 zwj. All rights reserved.
+
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+//双指针表示，top指向最高元素的后一位
+#define STACKSIZE 100
+typedef int EmelentType;
+typedef struct stackList
+{
+    EmelentType *top, *base;
+    int stackSize[STACKSIZE];
+    }stackList;
+
+
+void init(stackList *s)
+{
+
+    s->base=(EmelentType*)malloc(STACKSIZE*sizeof(EmelentType));
+    if (!s->base) {
+        return;
+    }
+
+    s->top=s->base;
+    s->stackSize=STACKSIZE;
+}
+
+
+void push(stackList*s, EmelentType e)
+{
+ 
+    if (s->top-s->base>=STACKSIZE) {
+        
+        s->base=(EmelentType*)realloc(s->base, s->stackSize+20);
+        s->top=s->base+s->stackSize;
+        s->stackSize=s->stackSize+20;
+    }
+    
+    *(s->top)=e;
+    s->top++;
+    
+}
+
+void pop(stackList *s,EmelentType *e)
+{
+    if (s->top<=s->base) {
+        return;
+    }
+    
+    s->top--;
+    e=*(s->top);
+}
+
+//---------------------------
+//top指向最高元素 ,非指针表示
+typedef struct stackNode
+{
+    EmelentType data[STACKSIZE];
+    int stackTop;
+}stackNode;
+
+void init2(stackNode *s)
+{
+
+    s->stackTop=-1;
+
+}
+
+void push2(stackNode *s,EmelentType e)
+{
+
+    if (s->stackTop>STACKSIZE-1) {
+        return;
+    }
+
+    s->data[++s->stackTop]=e;
+
+
+}
+void pop2(stackNode *s,EmelentType *e)
+{
+
+    if (s->stackTop<=-1) {
+        return;
+    }
+    *e=s->data[s->top--];
+  
+}
+
+//－－－－－－－－－－－－－－－－
+//链栈
+
+typedef struct listNode
+{
+    EmelentType data;
+    struct listNode* next;
+} listNode;
+
+
+typedef struct listStack
+{
+   struct listNode *top;
+    int size;
+}
+listStack;
+
+
+void push3(listStack *s, EmelentType e)
+{
+
+
+    listNode *p=(listNode *)malloc(sizeof(listNode));
+    p->data=e;
+    p->next=s->top;
+    s=p;
+    
+}
+
+void pop3(listStack *s,EmelentType *e)
+{
+    if (s->top==nullptr) {
+        return;
+    }
+    listNode *p=s->top;
+    *e=s->top->data;
+    s->top=s->top->next;
+    free(p);
+
+
+}
+
